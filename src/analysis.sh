b1fadb0ebea6ab21ff4929b0e1a5b386f07a1fb8
James Shen <jieran.shen@duke.edu>
2021-01-01 19:25:42 -0500

    Optimized Debug/Build procedure

---
+++ src/analysis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Sets snowglobes envrionemnt variable to the correct version, and reverts it back after it is done.
+legacy_snowglobes=/home/james/Shared/neutrino/snowglobes_legacy
+
+sys_snowglobes = $SNOWGLOBES
+export SNOWGLOBES=$legacy_snowglobes
+
+make analysis
+./analysis.o
+
+export SNOWGLOBES=$sys_snowglobes
